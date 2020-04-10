2a51fd0526e38b4563a655d435fff114b73b7bd4
Beta-23 <alfred.dominguez@gmail.com>
2020-04-10 00:20:52 -0700

    Test Babel Js code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,7 @@
 console.log('App is loading...');
+
+// JXS - JavaScript XML extensions
+
+var template = /*#__PURE__*/React.createElement("p", null, "This is JXS from app.js");
+let appRoot = document.getElementById(app);
+ReactDOM.render(template, appRoot);
