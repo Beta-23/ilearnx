8b9d0e3aa9c14e12378a524a2d582a5ddddbf521
Beta-23 <alfred.dominguez@gmail.com>
2020-04-15 22:24:33 -0700

    Test let and const variable declarations

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,33 +1,31 @@
-'use strict';
-
 // Var Can be re-assigned and scoped to a function
 var nameVar = 'Bryant';
 var nameVar = 'Mike';
-console.log('Hi, ' + nameVar);
+console.log(`Hi, ${nameVar}`);
 
 function getPetName() {
-    var petName = 'Larry';
+    const petName = 'Larry';
     return petName;
 }
 // petName is in the Global scope here
-var petName = getPetName();
+const petName = getPetName();
 console.log(petName);
 
 // Can't re-define a variable with let and const
-var nameLet = 'Jet';
+let nameLet = 'Jet';
 nameLet = 'Logan';
-console.log('Hi, ' + nameLet);
+console.log(`Hi, ${nameLet}`);
 
-var nameConst = 'Ed';
-console.log('Hi, ' + nameConst);
+const nameConst = 'Ed';
+console.log(`Hi, ${nameConst}`);
 
 // Block scoping, prevent escape of variables (expect UUID to print twice)
 
-var fullName = 'UUID DID';
-var firstName = void 0;
+const fullName = 'UUID DID';
+let firstName;
 
 if (fullName) {
     firstName = fullName.split(' ')[0];
     console.log(firstName);
 }
-console.log(firstName);
+ console.log(firstName)
