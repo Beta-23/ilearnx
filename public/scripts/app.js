f851be241969f788b348ff8149ccc4ff1c95de51
Beta-23 <alfred.dominguez@gmail.com>
2020-04-21 15:40:10 -0700

    Update app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,81 @@
 'use strict';
 
-// arguments object - no longer bound with arrow funtions
+console.log('App is loading...Testing babel');
 
-var add = function add(a, b) {
-    // console.log(arguments);
-    return a + b;
+// JXS - JavaScript XML extensions
+
+var appObj = {
+    title: 'iLearnx App',
+    subtitle: 'What Should I learn next?',
+    options: ['React']
+
 };
 
-console.log(add(1, 2));
-
-// this keyword - no longer bound
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        appObj.title
+    ),
+    appObj.subtitle && React.createElement(
+        'p',
+        null,
+        appObj.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        appObj.options.length > 0 ? 'Here are your options' : 'No Options',
+        ':'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement(
+            'li',
+            null,
+            appObj.options
+        )
+    )
+);
 
 var user = {
-    name: 'Jordan',
-    cities: ['PA', 'NY', 'WA'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-    }
-};
-// call the user object
-console.log(user.printPlacesLived());
-
-var multiplier = {
-    numbers: [5, 8, 4],
-    multiplyBy: 5,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (newnum) {
-            return _this2.multiplyBy * newnum;
-        });
-    }
+    name: 'Bryant',
+    age: 26,
+    location: 'L.A.'
 };
 
-console.log(multiplier.multiply());
+function getLocation(location) {
+    // check for user location conditional
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+    // console.log('no location found');
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
