a5c80eb20f2d8d607bb49157e9e4ae9688246b6e
Beta-23 <alfred.dominguez@gmail.com>
2020-04-29 22:03:39 -0700

    Create the instance of a person

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,16 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Person = function Person(name) {
+var Person = function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+
     _classCallCheck(this, Person);
 
     this.name = name;
 };
 
 var me = new Person('Andi Ann');
-
 console.log(me);
+
+var other = new Person();
+console.log(other);
