116502b8c83413a2bf4a9954d31fb0ce2583de02
Beta-23 <alfred.dominguez@gmail.com>
2020-05-01 21:28:20 -0700

    Create the person greeting class

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -2,10 +2,13 @@ class Person {
     constructor(name = 'Anonymous') {
         this.name = name;
     }
+    getGreeting() {
+        return ' Hi ' + this.name + '!';
+    }
 }
 
 const me = new Person('Andi Ann');
-console.log(me);
+console.log(me.getGreeting());
 
 const other = new Person();
-console.log(other);
+console.log(other.getGreeting());
