b69f0ec4a91a709298906a812b1c4bea3816378d
Beta-23 <alfred.dominguez@gmail.com>
2020-05-02 21:20:00 -0700

    Test the getDescription method logic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -9,58 +11,70 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Person = function () {
-    function Person() {
-        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
-        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-        _classCallCheck(this, Person);
+    _classCallCheck(this, Person);
 
-        this.name = name;
-        this.age = age;
+    this.name = name;
+    this.age = age;
+  }
+
+  _createClass(Person, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      return 'Hi. I am ' + this.name + '!';
     }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' year(s) old.';
+    }
+  }]);
 
-    _createClass(Person, [{
-        key: 'getGreeting',
-        value: function getGreeting() {
-            return 'Hi I am ' + this.name + '!';
-        }
-    }, {
-        key: 'getDescription',
-        value: function getDescription() {
-            return 'Hi, this is ' + this.name + ' and I am ' + this.age + ' years old';
-        }
-    }]);
-
-    return Person;
+  return Person;
 }();
-
-// subclass of the Person class  
+// subclass of the Person class 
 
 
 var Student = function (_Person) {
-    _inherits(Student, _Person);
+  _inherits(Student, _Person);
 
-    function Student(name, age, major) {
-        _classCallCheck(this, Student);
+  function Student(name, age, major) {
+    _classCallCheck(this, Student);
 
-        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+    var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
 
-        _this.major = major;
-        return _this;
+    _this.major = major;
+    return _this;
+  }
+  // check if student has a major
+
+
+  _createClass(Student, [{
+    key: 'hasMajor',
+    value: function hasMajor() {
+      return !!this.major;
     }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
 
-    _createClass(Student, [{
-        key: 'hasMajor',
-        value: function hasMajor() {
-            return !!this.major;
-        }
-    }]);
+      if (this.hasMajor()) {
+        description += ' My major is ' + this.major + '.';
+      }
 
-    return Student;
+      return description;
+    }
+  }]);
+
+  return Student;
 }(Person);
 
 var me = new Student('Andi Ann', 26, 'Computer Science');
-console.log(me.hasMajor());
+console.log(me.getDescription());
 
 var other = new Student();
-console.log(other.hasMajor());
+console.log(other.getDescription());
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,31 +1,39 @@
 class Person {
-    constructor(name = 'Anonymous', age= 0) {
-        this.name = name;
-        this.age = age;
+    constructor(name = 'Anonymous', age = 0) {
+      this.name = name;
+      this.age = age;
     }
     getGreeting() {
-        return `Hi I am ${this.name}!`;
+      return `Hi. I am ${this.name}!`;
     }
     getDescription() {
-        return `Hi, this is ${this.name} and I am ${this.age} years old`;
+      return `${this.name} is ${this.age} year(s) old.`;
     }
-}  
-
-// subclass of the Person class  
-class Student extends Person {
+  }
+  // subclass of the Person class 
+  class Student extends Person {
     constructor(name, age, major) {
-        super(name, age);
-        this.major = major;
+      super(name, age);
+      this.major = major;
     }
-
+    // check if student has a major
     hasMajor() {
-        return !!this.major
+      return !!this.major;
     }
-}
+    getDescription() {
+      let description = super.getDescription();
+  
+      if (this.hasMajor()) {
+        description += ` My major is ${this.major}.`;
+      }
+  
+      return description;
+    }
+  }
 
 
 const me = new Student('Andi Ann', 26, 'Computer Science');
-console.log(me.hasMajor());
+console.log(me.getDescription());
 
 const other = new Student();
-console.log(other.hasMajor());
+console.log(other.getDescription());
