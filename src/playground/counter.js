b32146b57da9d73427974c04d4757596f3105a28
Beta-23 <alfred.dominguez@gmail.com>
2020-05-13 15:46:39 -0700

    Clean counter code

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -53,39 +53,3 @@ class Counter extends React.Component {
 
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
-
-
-// let count =0;
-
-// const addOne = ( ) => {
-//     count++;
-//     renderCounterApp();
-// }
-
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// }
-
-// const reSet = () => {
-//     count = 0;
-//     renderCounterApp();
-// }
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <br />
-//             <br />
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reSet}>Reset</button>
-//         </div>
-//     );
-//     ReactDOM.render(templateTwo, appRoot);
-// }
-
-// renderCounterApp()
