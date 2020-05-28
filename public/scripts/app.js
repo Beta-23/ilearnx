4da4e1ea0ce0cac8b4278d264887be89e67865cb
Beta-23 <alfred.dominguez@gmail.com>
2020-05-28 15:37:14 -0700

    Refactor code to use local modules to run babel and live-server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,77 +8,253 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Counter = function (_React$Component) {
-    _inherits(Counter, _React$Component);
+var IlearnxApp = function (_React$Component) {
+    _inherits(IlearnxApp, _React$Component);
 
-    function Counter(props) {
-        _classCallCheck(this, Counter);
+    function IlearnxApp(props) {
+        _classCallCheck(this, IlearnxApp);
 
-        var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+        var _this = _possibleConstructorReturn(this, (IlearnxApp.__proto__ || Object.getPrototypeOf(IlearnxApp)).call(this, props));
 
-        _this.handleAddOne = _this.handleAddOne.bind(_this);
-        _this.handleMinusOne = _this.handleMinusOne.bind(_this);
-        _this.handleReset = _this.handleReset.bind(_this);
+        _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
+        _this.handlePickOptions = _this.handlePickOptions.bind(_this);
+        _this.handleAddOption = _this.handleAddOption.bind(_this);
+        _this.handleDeleteIndividualOption = _this.handleDeleteIndividualOption.bind(_this);
         _this.state = {
-            count: 0
+            options: []
         };
         return _this;
     }
-
     // Lifecycle methods
 
 
-    _createClass(Counter, [{
+    _createClass(IlearnxApp, [{
         key: 'componentDidMount',
         value: function componentDidMount() {
-            var stringCount = localStorage.getItem('count');
-            var count = parseInt(stringCount, 10);
+            try {
+                var json = localStorage.getItem('options');
+                var options = JSON.parse(json);
 
-            if (!isNaN(count)) {
-                this.setState(function () {
-                    return { count: count };
-                });
-                console.log('Component mounted sucessfully!');
+                if (option) {
+                    this.setState(function () {
+                        return { options: options };
+                    });
+                }
+            } catch (e) {
+                // Do nothing at all  
             }
         }
     }, {
         key: 'componentDidUpdate',
         value: function componentDidUpdate(prevProps, prevState) {
-            if (prevState.count !== this.state.count) {
-                localStorage.setItem('count', this.state.count);
-                console.log('Saving count Data to Local Storage!');
+            if (prevState.options.length !== this.state.options.length) {
+                var json = JSON.stringify(this.state.options);
+                localStorage.setItem('options', json);
+                console.log('Saving Data to Local Storage!');
             }
         }
     }, {
-        key: 'handleAddOne',
-        value: function handleAddOne() {
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count + 1
-                };
-            });
-            console.log('handleAddOne updated state');
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            console.log('App WillUnmount!');
         }
+
+        // Event handler implicit setState object
+
     }, {
-        key: 'handleMinusOne',
-        value: function handleMinusOne() {
-            this.setState(function (prevState) {
-                if (prevState.count >= 1) return {
-                    count: prevState.count - 1
-                };
-                return alert('Count is at Zero');
-            });
-            console.log('handleMinusOne updated state');
-        }
-    }, {
-        key: 'handleReset',
-        value: function handleReset() {
+        key: 'handleDeleteOptions',
+        value: function handleDeleteOptions() {
             this.setState(function () {
+                return { options: [] };
+            });
+        }
+    }, {
+        key: 'handleDeleteIndividualOption',
+        value: function handleDeleteIndividualOption(optionToRemove) {
+            this.setState(function (prevState) {
                 return {
-                    count: 0
+                    options: prevState.options.filter(function (option) {
+                        return optionToRemove !== option;
+                    })
                 };
             });
-            console.log('handleReset updated state');
+        }
+        // Event handler
+
+    }, {
+        key: 'handlePickOptions',
+        value: function handlePickOptions() {
+            var randomNum = Math.floor(Math.random() * this.state.options.length);
+            var option = this.state.options[randomNum];
+            return alert('Our pick is: ' + option);
+        }
+        // Event handler from child
+
+    }, {
+        key: 'handleAddOption',
+        value: function handleAddOption(option) {
+            if (!option) {
+                return 'Enter a valid value for language choice!';
+            } else if (this.state.options.indexOf(option) > -1) {
+                return alert('This language is already in you choices!');
+            }
+
+            this.setState(function (prevState) {
+                return {
+                    options: prevState.options.concat(option)
+                };
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var subtitle = 'Let an al·go·rithm show your learning path!';
+
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(Header, { subtitle: subtitle }),
+                React.createElement(Action, {
+                    hasOptions: this.state.options.length > 0,
+                    handlePickOptions: this.handlePickOptions
+                }),
+                React.createElement(Options, {
+                    options: this.state.options,
+                    handleDeleteOptions: this.handleDeleteOptions,
+                    handleDeleteIndividualOption: this.handleDeleteIndividualOption
+                }),
+                React.createElement('br', null),
+                React.createElement(AddOption, {
+                    handleAddOption: this.handleAddOption
+                })
+            );
+        }
+    }]);
+
+    return IlearnxApp;
+}(React.Component);
+
+// Header stateless functional component for User
+
+
+var Header = function Header(props) {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            props.title
+        ),
+        props.subtitle && React.createElement(
+            'h2',
+            null,
+            props.subtitle
+        )
+    );
+};
+
+Header.defaultProps = {
+    title: 'iLearnx App'
+
+    // Action stateless functional component for User
+};var Action = function Action(props) {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'button',
+            {
+                onClick: props.handlePickOptions,
+                disabled: !props.hasOptions
+            },
+            'What langueage should I learn?'
+        )
+    );
+};
+
+// Options stateless functional component for User
+var Options = function Options(props) {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'p',
+            null,
+            props.options.length > 0 ? 'Here Are Your Options:' : 'No Options!'
+        ),
+        props.options.map(function (option) {
+            return React.createElement(Option, {
+                key: option,
+                optionText: option,
+                handleDeleteIndividualOption: props.handleDeleteIndividualOption
+            });
+        }),
+        React.createElement('br', null),
+        React.createElement(
+            'button',
+            {
+                disabled: props.options.length === 0,
+                onClick: props.handleDeleteOptions
+            },
+            'Remove All'
+        ),
+        props.options.length === 0 && React.createElement(
+            'p',
+            null,
+            'Please Add Your Langueage Choice to get Started!'
+        )
+    );
+};
+
+// Option stateless functional component for User
+var Option = function Option(props) {
+    return React.createElement(
+        'div',
+        null,
+        props.optionText,
+        React.createElement(
+            'button',
+            {
+                onClick: function onClick(e) {
+                    props.handleDeleteIndividualOption(props.optionText);
+                }
+            },
+            'Remove Item'
+        )
+    );
+};
+
+var AddOption = function (_React$Component2) {
+    _inherits(AddOption, _React$Component2);
+
+    function AddOption(props) {
+        _classCallCheck(this, AddOption);
+
+        var _this2 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
+
+        _this2.handleAddOption = _this2.handleAddOption.bind(_this2);
+        _this2.state = {
+            error: undefined
+        };
+        return _this2;
+    }
+
+    _createClass(AddOption, [{
+        key: 'handleAddOption',
+        value: function handleAddOption(e) {
+            e.preventDefault();
+
+            var option = e.target.elements.option.value.trim();
+            var error = this.props.handleAddOption(option);
+
+            this.setState(function () {
+                return { error: error };
+            });
+
+            if (!error) {
+                e.target.elements.option.value = '';
+            }
         }
     }, {
         key: 'render',
@@ -86,33 +262,26 @@ var Counter = function (_React$Component) {
             return React.createElement(
                 'div',
                 null,
-                React.createElement(
-                    'h1',
+                this.state.error && React.createElement(
+                    'p',
                     null,
-                    'Count: ',
-                    this.state.count
+                    this.state.error
                 ),
                 React.createElement(
-                    'button',
-                    { onClick: this.handleAddOne },
-                    '+1'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleMinusOne },
-                    '-1'
-                ),
-                React.createElement('br', null),
-                React.createElement(
-                    'button',
-                    { onClick: this.handleReset },
-                    'Reset'
+                    'form',
+                    { onSubmit: this.handleAddOption },
+                    React.createElement('input', { type: 'text', name: 'option' }),
+                    React.createElement(
+                        'button',
+                        null,
+                        'Add Your Langueage'
+                    )
                 )
             );
         }
     }]);
 
-    return Counter;
+    return AddOption;
 }(React.Component);
 
-ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(IlearnxApp, null), document.getElementById('app'));
