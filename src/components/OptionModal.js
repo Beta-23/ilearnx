20fe91a58d443a2779ae0e7f37ea2511e721d5b3
Beta-23 <alfred.dominguez@gmail.com>
2020-06-05 19:36:23 -0700

    Improve UI by implementing onRequest handler for modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,6 +5,7 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
     <Modal 
     isOpen={ !!props.selectedOption }
+    onRequestClose={props.handleClearSelectedOption}
     contentLabel="Selected Option"
     >
         <h3>Modal text here</h3>
