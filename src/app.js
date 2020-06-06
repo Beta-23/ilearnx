dd5063ecdd68cb10694c0ce301e106209e3fd72e
Beta-23 <alfred.dominguez@gmail.com>
2020-06-05 17:41:17 -0700

    Install the react-modal library

--- src/app.js
+++ src/app.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import IlearnxApp from './components/IlearnxApp';
+import IlearnxApp from './components/IlearnxApp';
 
-const Layout = (props) => {
-    return (
-        <div>
-            <p>Header</p>
-            {props.children}
-            <p>Footer</p>
-        </div>
-    ); 
-};
-
-//inline child render
-ReactDOM.render((
-    <Layout>
-        <h1>New page Title</h1>
-        <p>New page ctx</p>
-    </Layout>    
-    ), document.getElementById('app'));
+  
+ReactDOM.render(<IlearnxApp />, document.getElementById('app'));
 
 
 
---
+++ src/components/OptionModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Modal from 'react-modal';
+
+const OptionModal = () => {
+    return (
+        <div>
+            modal text here
+        </div>
+    )
+};
+
