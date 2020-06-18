26c9ee569f1210d9b59c25aa014b8e1dbf25f76e
Beta-23 <alfred.dominguez@gmail.com>
2020-06-17 22:54:45 -0700

    Create the data component and json data

--- src/components/LangList.js
+++ src/components/LangList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import DataList from '../data/list.json'
 
+
 // Action stateless functional component
 const LangList = (props) => (
+        
         <div>
             <ul>
             {DataList.map((listDetail, index) => {
